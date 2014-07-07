78c453684f4be25e18758917b74bb618f966075e
Hicham Chourak <hichamchourak@Hichams-Mac.local>
2014-07-07 20:43:31 +0200

    Initial Commit

---
+++ BreakingBricks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BreakingBricks
+//
+//  Created by Hicham Chourak on 07/07/14.
+//  Copyright (c) 2014 Hicham Chourak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
